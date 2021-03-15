71725bee9563a96324ad2024b3924e959523fd60
Bognaum <bognaum@gmail.com>
2021-03-15 21:36:41 +0200

    Renamed and added index.js .

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-import describeAPI from "./describeAPI.js";
-import SyntaxHighlightAPI from "./SyntaxHighlightAPI.js";
-
-export default {
-	version: "3.0.0-alpha",
-	describeAPI,
-	SyntaxHighlightAPI, // (mainRule, clPref="syntax-hl-fk")
-};
+import syntaxHlFk from "./syntax-hl-fk.js";
+export default syntaxHlFk;
---
+++ syntax-hl-fk.js
@@ -0,0 +1,8 @@
+import describeAPI from "./describeAPI.js";
+import SyntaxHighlightAPI from "./SyntaxHighlightAPI.js";
+
+export default {
+	version: "3.0.0-alpha",
+	describeAPI,
+	SyntaxHighlightAPI, // (mainRule, clPref="syntax-hl-fk")
+};
