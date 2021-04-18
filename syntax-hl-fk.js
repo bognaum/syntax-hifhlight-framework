32e44f20ad6e79ff0ff61923ebaed702c1b712da
Bognaum <bognaum@gmail.com>
2021-04-19 00:52:37 +0300

    4.0.3

--- syntax-hl-fk.js
+++ syntax-hl-fk.js
@@ -2,7 +2,7 @@ import describeAPI from "./describeAPI.js";
 import Highlighter from "./Highlighter.js";
 
 export default {
-	version: "4.0.1",
+	version: "4.0.3",
 	describeAPI,
 	Highlighter, // (mainRule, clPref="syntax-hl-fk")
 };
